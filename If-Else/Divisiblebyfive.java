c987291c0161fca4f07e2943a1c6d970e14c01da
vaibhav450-er <vaibhavkeni450@gmail.com>
2026-03-07 22:54:28 +0530

    initial commit

---
+++ If-Else/Divisiblebyfive.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Divisiblebyfive {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+
+		System.out.println("Enter the number");
+
+		int n=sc.nextInt();
+
+		if(n%5==0) 
+
+			System.out.println("number is divisible by 5");
+		else
+
+			System.out.println("number is not divisible by 5");
+
+}
+    
+}
