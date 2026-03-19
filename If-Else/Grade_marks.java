b15e360cfbb747e5b6d24be80e6d315186c92367
vaibhav450-er <vaibhavkeni450@gmail.com>
2026-03-20 02:19:22 +0530

    initial commit

---
+++ If-Else/Grade_marks.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Grade_marks {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int marks=sc.nextInt();
+        if(marks>=90) {
+        	System.out.println("Grade A");
+        	
+        }
+        else if(marks>=75) {
+        	System.out.println("Grade B");
+        	
+        }
+        else if(marks>=60) {
+        	System.out.println("Grade C");
+        	
+        }
+        else {
+        	System.out.println("Grade D");
+        	
+        }
+        sc.close();
+			}
+}
