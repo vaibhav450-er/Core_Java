47ab90b3527c536e0ee83209e4af6b5305febd48
vaibhav450-er <vaibhavkeni450@gmail.com>
2026-03-22 22:07:44 +0530

    initial commit

---
+++ If-Else/Credentials.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Credentials {
+
+	public static void main(String[] args) {
+	
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter username");
+		String u=sc.next();
+		System.out.println("Enter password");
+		String p=sc.next();
+		String username="vaibhav";
+		String password="keni@123";
+		if(u.equals(username)&& p.equals(password)) {
+			System.out.println("Login Successful");
+		}
+		else {
+			System.out.println("Login Failed");
+		}
+}
+
+}
