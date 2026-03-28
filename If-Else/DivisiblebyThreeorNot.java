0b1821aa0be079aaacaa0a9ecc6b112f9ee844c6
vaibhav450-er <vaibhavkeni450@gmail.com>
2026-03-28 22:42:39 +0530

    initial commit

---
+++ If-Else/DivisiblebyThreeorNot.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class DivisiblebyThreeorNot{
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter number");
+		int a=sc.nextInt();
+		if(a%3==0) {
+			System.out.println(a+" is divisible by 3");
+		}
+		else {
+			System.out.println(a+" is divisible by 3");
+		}
+		sc.close();
+			}
+}
